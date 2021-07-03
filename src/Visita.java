1d320e94777f37c40b050d5b5b0e011654de17fd
RobotoSWAG <74079951+RobotoSWAG@users.noreply.github.com>
2021-07-03 09:43:46 -0400

    Sin Herencia

---
+++ src/Visita.java
@@ -0,0 +1,111 @@
+import java.util.Date;
+
+public class Visita {
+    String codVisita;
+    Date fechaHora;
+    Hospital hospital;
+    Servicio servicio;
+    Medico medico;
+    String diagnosticos;
+    String tratamiento;
+    int numeroCama;
+    Date fechaSalida;
+    Historial historial;
+
+    public Visita(String codVisita, Date fechaHora, Hospital hospital, Servicio servicio, Medico medico, String diagnosticos, String tratamiento, int numeroCama, Date fechaSalida, Historial historial){
+        this.codVisita = codVisita;
+        this.fechaHora = fechaHora;
+        this.hospital = hospital;
+        this.servicio = servicio;
+        this.medico = medico;
+        this.diagnosticos = diagnosticos;
+        this.tratamiento = tratamiento;
+        this.numeroCama = numeroCama;
+        this.fechaSalida = fechaSalida;
+        this.historial = historial;
+    }
+
+    public String getCodVisita(){
+        return codVisita;
+    }
+
+    public void setCodVisita(String codVisita){
+        this.codVisita = codVisita;
+    }
+
+    public Date getFechaHora(){
+        return fechaHora;
+    }
+
+    public void setFechaHora(Date fechaHora){
+        this.fechaHora = fechaHora;
+    }
+
+    public Hospital getHospital(){
+        return hospital;
+    }
+
+    public void setHospital(Hospital hospital){
+        this.hospital = hospital;
+    }
+
+    public Servicio getServicio(){
+        return servicio;
+    }
+
+    public void setServicio(Servicio servicio){
+        this.servicio = servicio;
+    }
+
+    public Medico getMedico(){
+        return medico;
+    }
+
+    public void setMedico(Medico medico){
+        this.medico = medico;
+    }
+
+    public String getDiagnosticos(){
+        return diagnosticos;
+    }
+
+    public void setDiagnosticos(String diagnosticos){
+        this.diagnosticos = diagnosticos;
+    }
+
+    public String getTratamiento(){
+        return tratamiento;
+    }
+
+    public void setTratamiento(String tratamiento){
+        this.tratamiento = tratamiento;
+    }
+
+    public int getNumeroCama(){
+        return numeroCama;
+    }
+
+    public void setNumeroCama(int numeroCama){
+        this.numeroCama = numeroCama;
+    }
+
+    public Date getFechaSalida(){
+        return fechaSalida;
+    }
+
+    public void setFechaSalida(Date fechaSalida){
+        this.fechaSalida = fechaSalida;
+    }
+
+    public Historial getHistorial(){
+        return historial;
+    }
+
+    public void setHistorial(Historial historial){
+        this.historial = historial;
+    }
+
+    public Visita(){
+
+    }
+}
